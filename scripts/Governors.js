b227da89c59f976370ca508f5b06dd4f5bd49015
collin <geniuscollin@gmail.com>
2021-10-26 14:36:27 -0500

    added code to generate html rep for governors and facilities

--- scripts/Governors.js
+++ scripts/Governors.js
@@ -3,3 +3,26 @@
 This module contains event listers and creates html for governors, and exports to Exomine.
 
 */
+import { getGovernors } from "/database.js"
+
+const governorsArray = getGovernors()
+
+export const governorHTML = () => {
+    let html = "<h2>choose a governor</h2>"
+
+    html += `<select id="governor">`
+    html += '<option value="0">choose a governor</option>'
+
+    const governorOptions = governorsArray.map( (governor) => {
+        return `<option value="${governor.id}">${governor.name}</option>`
+    })
+
+    html += governorOptions.join("")
+    html += "</select>"
+    return html
+
+}
+
+
+
+
