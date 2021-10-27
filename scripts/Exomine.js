b461ee8a7287298bf434d1bac1bffc645f8f653b
collin <geniuscollin@gmail.com>
2021-10-27 09:41:40 -0500

    needed to commit before the fetch

--- scripts/Exomine.js
+++ scripts/Exomine.js
@@ -1,15 +1,20 @@
 import { facilitiesHTML } from "./Facilities.js";
+import { facilityMineralHTML } from "./FacilityMinerals.js";
 import { governorHTML } from "./Governors.js";
 /*
 
 This module imports html from individual modules and exports to main.js.
 
 */
+
+
+facilityMineralHTML
 const facilitieshtml = facilitiesHTML()
 const governorhtml = governorHTML()
 
 export const renderHTML = () => {
     return `${facilitieshtml}
     ${governorhtml}
+    ${facilityMineralHTML()}
     `
 }
--- scripts/FacilityMinerals.js
+++ scripts/FacilityMinerals.js
@@ -4,3 +4,28 @@ FacilityMinerals is an intersection dbase for facilities and minerals.
 This module contains event listers and creates html for facilityMinerals, and exports to Exomine.
 
 */
+
+import { getMineralFacilities, getFacilities } from "./dataAccess.js"
+
+const facilityArray = getFacilities()
+const facilityMinerals = getMineralFacilities()
+
+
+export const facilityMineralHTML = () => {
+
+    let html = `<h2>facility mineral for ${facilityArray.name}</h2>`
+    html += "<ul>"
+
+    // This is how you have been converting objects to <li> elements
+    
+        
+            html += `<li>
+            <input type="radio" name="metal" value="1" /> minerallll
+        </li>`
+
+
+        html += "</ul>"
+        return html
+    }
+
+ 
