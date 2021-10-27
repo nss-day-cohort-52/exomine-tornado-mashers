3ab8f9e3a4040c0715fc87c90d48c71830ecb9c6
collin <geniuscollin@gmail.com>
2021-10-27 12:47:53 -0500

    l

--- scripts/AvailableResources.js
+++ scripts/AvailableResources.js
@@ -1,15 +1,25 @@
+import { getColonies } from "./dataAccess.js"
+
 /*
 
 AvaiableResources is an intersection dbase for colonies and minerals.
 This module contains event listers and creates html for AvailableResources, and exports to Exomine.
 
 */
+// get governer id user chose
+// get colonyId value from said governer
+// compare the colonyId to colony ids in colony array
+// display colony that matches colonyid
+
+
+
 
 export const availableResources =() => {
-    return `
-        <h2>Alderaan Minerals</h2>
-        <p>29 tons of Barab ore</p>
-        <p>50 tons of Guerrenrite</p>
-        <p>20 tons of Hollinium</p>
-    `
+
+    // return `${governer}`
+
+
+
+
+    
 }
--- scripts/Governors.js
+++ scripts/Governors.js
@@ -3,7 +3,21 @@
 This module contains event listers and creates html for governors, and exports to Exomine.
 
 */
-import { getGovernors } from "./dataAccess.js"
+import { getGovernors, setGovernor } from "./dataAccess.js"
+
+// create dispatch event that will have the message of governor chosen? 
+
+// document.addEventListener(
+//     "change",
+//     (event) => {
+//         if(event.target.id === "governor") {
+//             const gov = parseInt(event.target.value)
+//             document.dispatchEvent( new CustomEvent(gov)
+//             // setGovernor(parseInt(event.target.value))
+//         }
+//     }
+// )
+
 
 const governorsArray = getGovernors()
 
